1bee6e4cccad2698d035a8586ef611d7b1440c0a
tanuyru <146369378+tanuyru@users.noreply.github.com>
2024-05-15 13:50:21 -0600

    Added code to filter data by available properties

--- Riksdagen/FileSelector.cs
+++ Riksdagen/FileSelector.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Riksdagen.Import.ExportedModels;
+using Riksdagen.Import.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Riksdagen
@@ -45,5 +48,67 @@ namespace Riksdagen
                 }
             }
         }
+        public void CopyAndCreate(string inputDir, string outputDir, 
+            Dictionary<string, PropositionExportModel> dictionary,
+            Func<PropositionExportModel, bool> filterFunc = null)
+        {
+            Func<string, HtmlParsedResult> creatorFunc = null;
+            if (creatorFunc == null)
+            {
+                creatorFunc = (s) => JsonSerializer.Deserialize<HtmlParsedResult>(s);
+            }
+
+            if (filterFunc == null)
+            {
+                filterFunc = (f) => !string.IsNullOrEmpty(f.Organ);
+            }
+
+            if (!Directory.Exists(outputDir))
+            {
+                Directory.CreateDirectory(outputDir);
+            }
+            int numTotal = 0;
+            int numCopied = 0;
+            int numFailed = 0;
+            foreach (var f in Directory.GetFiles(inputDir))
+            {
+                numTotal++;
+                try
+                {
+                    var jsonText = File.ReadAllText(f);
+                    var id = Path.GetFileNameWithoutExtension(f).ToLower();
+
+                    var obj = creatorFunc(jsonText);
+                    var res = dictionary[id]; // just throw exception
+
+                    if (!string.IsNullOrEmpty(res.Organ))
+                    { 
+                        res.ParsedResult = obj;
+
+                    var outputName = outputDir + Path.GetFileName(f);
+                        var transformedJson = JsonSerializer.Serialize(res);
+                        File.WriteAllText(outputName, transformedJson);
+                        numCopied++;
+                        Console.WriteLine("Found file, "+numTotal+" and copied " + outputName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    numFailed++;
+                    Console.WriteLine("ERROR processing file: " + f);
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            Console.WriteLine("Done with " + numCopied + "/" + numTotal);
+        }
+        public static PropositionExportModel TransformFunc(HtmlParsedResult input, string fileName, Dictionary<string, PropositionExportModel> dic)
+        {
+            var id = Path.GetFileNameWithoutExtension(fileName).ToLower();
+            var existing = dic[id];
+            existing.ParsedResult = input;
+            return existing;
+        }
+
+      
     }
 }
--- Riksdagen/Program.cs
+++ Riksdagen/Program.cs
@@ -2,6 +2,7 @@
 using HtmlAgilityPack;
 using Riksdagen;
 using Riksdagen.Import;
+using Riksdagen.Import.ExportedModels;
 using Riksdagen.Import.Models;
 using System.Net;
 using System.Text.Json;
@@ -15,7 +16,14 @@ var outputDir = @"C:\Users\whydo\OneDrive\Documentos\ml\riksdagen\propositionert
 var emptyDir = @"C:\Users\whydo\OneDrive\Documentos\ml\riksdagen\propositionertxt\empty\";
 var failDir = @"C:\Users\whydo\OneDrive\Documentos\ml\riksdagen\propositionertxt\failed\";
 
-var copyInput = @"C:\Users\whydo\OneDrive\Documentos\ml\riksdagen\propositionertxt\parsed\";
-var copyOutput = @"C:\Users\whydo\OneDrive\Documentos\ml\riksdagen\propositionertxt\parsed\summary\";
+var copyInput = @"C:\Users\whydo\OneDrive\Documentos\ml\riksdagen\propositionertxt\parsed\summary\";
+var copyOutput = @"C:\Users\whydo\OneDrive\Documentos\ml\riksdagen\propositionertxt\parsed\summary\organ";
 
-new FileSelector().CopyFiles<HtmlParsedResult>(copyInput, copyOutput, (r) => !string.IsNullOrEmpty(r.Summary));
+var csvInput = @"C:\Users\whydo\OneDrive\Documentos\ml\riksdagen\propscsv\";
+var propJsonData = new CsvImporter().ImportBaseFromCsv(csvInput);
+
+Console.WriteLine("Loaded " + propJsonData?.Count + " element from csv file " + csvInput);
+
+var dictionary = propJsonData.ToDictionary(m => m.DokumentId.ToLower(), m => m);
+new FileSelector().CopyAndCreate(copyInput, copyOutput, dictionary);
+
